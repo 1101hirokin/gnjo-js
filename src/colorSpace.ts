6868ebdd3857d070e77b86de2f0dbfab0c87f26d
1101hiroki_n <1101hiroki.n@gmail.com>
2023-02-19 16:00:39 +0900

    fix: Hue calclation logic and tests

--- src/colorSpace.ts
+++ src/colorSpace.ts
@@ -93,36 +93,41 @@ export class RGBASpace implements ColorSpace {
     let r = this.r / 255
     let g = this.g / 255
     let b = this.b / 255
-    
+
     const max = Math.max(r, g, b)
     const min = Math.min(r, g, b)
-
     const diff = max - min
 
     let h = 0
     let s = 0
-    let l = 0
+    let l = (max + min) / 2
 
     if (diff === 0) {
       h = 0
-    } else if (max === r) {
-      h = ((g - b) / diff) % 6
-    } else if (max === g) {
-      h = (b - r) / diff + 2
-    } else {
-      h = (r - g) / diff + 4
+      s = 0
+    }
+    switch (max) {
+      case r:
+        h = (g - b) / diff + (g < b ? 6 : 0)
+        break
+      case g:
+        h = (b - r) / diff + 2
+        break
+      case b:
+        h = (r - g) / diff + 4
+        break
     }
     h /= 6
+    h *= 360
     s = (diff === 0) ? 0 : diff / (1 - Math.abs(2 * l - 1))
 
     return new HSLASpace(
-      { value: h * 360, unit: "", },
+      { value: (isNaN(h)? 0 : h), unit: "", },
       s,
       l,
       this.alpha,
     )
 
-    return new HSLASpace({ value: h, unit: "deg" }, s, l, this.alpha)
   }
   public toHWB(): HWBSpace {
     const max = Math.max(this.r, this.g, this.b)
