fca07755b6d3691130ef410e2d7272bf73c4de52
1101hiroki_n <1101hiroki.n@gmail.com>
2023-03-05 18:16:45 +0900

    commit

--- src/main.ts
+++
@@ -1,7 +0,0 @@
-export * from './token';
-export * from './lexer';
-export * from './parser';
-export * from './evaluator';
-export * from './utils';
-export * from './colorProcessing';
-export * from './colorSpace';
